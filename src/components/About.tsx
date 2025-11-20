21c2fb3762e9b930941f0b9f1e86731b228ad92e
1wasilah1 <m.wasilahhadi@gmail.com>
2025-11-20 20:44:36 +0700

    perbaikna

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -61,12 +61,16 @@ export default function About() {
               {t('servedCountries')}
             </p>
 
-            <button className="bg-black text-white px-6 py-3 rounded font-semibold hover:bg-gray-800 transition-colors flex items-center space-x-2">
+            <a 
+              href="/compro.pdf" 
+              download="Save-Decor-Company-Profile.pdf"
+              className="bg-black text-white px-6 py-3 rounded font-semibold hover:bg-gray-800 transition-colors flex items-center space-x-2 inline-flex"
+            >
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
               </svg>
               <span>{t('downloadProfile')}</span>
-            </button>
+            </a>
           </div>
         </div>
       </div>
